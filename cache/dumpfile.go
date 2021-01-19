df897ca17fad0c83e659f1bd3e6a624bbc68d14e
Ldc <ldcc@live.com>
2021-01-19 17:04:14 +0800

    协程调度 -> 锁调度

--- cache/dumpfile.go
+++ cache/dumpfile.go
@@ -1,43 +1,33 @@
 package cache
 
+import "sync"
+
 type DumpFile interface {
 	Lookup(key string) interface{}
 	Extend(key string)
 }
 
 type Dump struct {
-	pipe chan string
+	sync.RWMutex
 	dmap map[string]interface{}
 }
 
 func NewDump() *Dump {
 	dump := new(Dump)
-	dump.pipe = make(chan string)
 	dump.dmap = make(map[string]interface{})
-	go func() {
-		for {
-			select {
-			case key := <-dump.pipe:
-				if key == "" {
-					continue
-				}
-				dump.dmap[key] = struct{}{}
-			}
-		}
-	}()
 	return dump
 }
 
 func (dump *Dump) Lookup(key string) interface{} {
 	val, exist := dump.dmap[key]
-	// TODO 查找本地文件
 	if !exist {
-		dump.pipe <- ""
-		val, _ = dump.dmap[key]
+		// TODO 查找本地文件
 	}
 	return val
 }
 
 func (dump *Dump) Extend(key string) {
-	dump.pipe <- key
+	dump.RLock()
+	dump.dmap[key] = struct{}{}
+	dump.RUnlock()
 }
